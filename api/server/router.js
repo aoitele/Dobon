c38196895a049647314d9f2b20a6885ef2e90e66
Takuto Eto <aoitele@gmail.com>
2021-02-23 16:03:01 +0900

    Add nodemon to APIcontainer

--- api/server/router.js
+++ api/server/router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const wrap = fn => (...args) => fn(...args).catch(args[2]);
 
 router.get("/", (req, res) => {
   res.send("Hello Router");
@@ -8,7 +9,22 @@ router.get("/", (req, res) => {
 router.get("/login", (req, res) => {});
 router.get("/logout", (req, res) => {});
 router.get("/create", (req, res) => {});
-router.get("/room", (req, res) => {});
+router.get("/room", (req, res) => {
+  // 開催中のルーム一覧を返す
+  const roomInfo = [
+    {
+      id: 1,
+      name: "テスト部屋A",
+      participantUserId: [1, 2, 3]
+    },
+    {
+      id: 2,
+      name: "テスト部屋F",
+      participantUserId: [4, 5, 6]
+    },
+  ];
+  res.json(roomInfo);
+});
 router.get("/room/:id", (req, res) => {
   res.send("this room number is" + req.params.id);
 });
