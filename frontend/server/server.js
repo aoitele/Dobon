5a91d1b2cacabdae757f2fd51b158f9045b004c2
Takuto Eto <aoitele@gmail.com>
2021-03-06 14:25:45 +0900

    ポート指定を修正

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
-const HOST = "localhost";
+const PORT = process.env.PORT || 3000;
+const HOST = (process.env === 'production') ? 'ymltest.herokuapp.com': 'localhost';
 
 app.listen(PORT);
 console.log(`running on http://${HOST}:${PORT}`);
