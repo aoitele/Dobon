17aca380291b37f240f145e02e4bbd78674472ca
Takuto Eto <aoitele@gmail.com>
2021-03-06 15:02:46 +0900

    heroku起動時のサーブ元を/distに修正

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -1,9 +1,12 @@
 "use strict";
 const express = require("express");
+const serveStatic = require('serve-static');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-const HOST = (process.env === 'production') ? 'ymltest.herokuapp.com': 'localhost';
+app.use(serveStatic("./dist"));
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
+
+const HOST = (process.env === 'production') ? 'ymltest.herokuapp.com': 'localhost';
 console.log(`running on http://${HOST}:${PORT}`);
