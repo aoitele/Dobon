3cf8b5977cc9961d35f2ac958c87a1c1784ed4f4
Takuto Eto <aoitele@gmail.com>
2021-03-07 01:03:55 +0900

    heroku.yml修正

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const serveStatic = require('serve-static');
 const app = express();
 
-app.use(serveStatic("./dist"));
+app.use(serveStatic("dist"));
 
 const PORT = process.env.PORT || 3000;
 const HOST = (process.env.NODE_ENV === 'production') ? 'https://ymltest.herokuapp.com': 'http://localhost';
