050740e0368fb4132125419b4b6a55dc7c1123c9
Takuto Eto <aoitele@gmail.com>
2021-03-14 13:32:30 +0900

    process env test2

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,9 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = process.env.API_BASE_URL;
 console.log(axios.defaults.baseURL, 'axios.defaults.baseURL');
 
-axios.defaults.baseURL = process.env.API_BASE_URL_TEST;
-console.log(axios.defaults.baseURL, 'axios.defaults.baseURLTEST');
-
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -43,10 +43,12 @@ export default {
       this.logging = true;
       try {
         //const res = await this.$axios.get('/api/login', { params:{ invitationcode: this.invitationCode }})
-        console.log(process.env.API_BASE_URL, 'API_BASE_URL')
-        console.log(process.env.API_BASE_URL_TEST, 'API_BASE_URL_TEST')
-        const res = await this.$axios.get('https://dobon-api.herokuapp.com/room');
-        console.log(res, 'res')
+        console.log(process.env.API_BASE_URL, 'API_BASE_URL');
+        const res1 = await this.$axios.get('/room');
+        console.log(res1, 'res1');
+
+        const res2 = await this.$axios.get(process.env.API_BASE_URL + '/room');
+        console.log(res2, 'res2');
       } catch(e) {
         console.log(e, 'e')
       }
