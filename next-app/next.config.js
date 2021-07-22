3ed94c1f223c990d7a9aa441e3af8de9539f5654
Takuto Eto <aoitele@gmail.com>
2021-07-22 17:51:47 +0900

    fix Access-Control-Allow-Origin

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -6,7 +6,7 @@ module.exports = {
           source: "/api/:path*",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: 'http://localhost:3000, https://dobon-frontend.vercel.app' },
+            { key: "Access-Control-Allow-Origin", value: 'http://localhost:3000, https://dobon-frontend.vercel.app/' },
             { key: "Vary", value: 'Origin' },
             { key: "Access-Control-Allow-Methods", value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           ]
