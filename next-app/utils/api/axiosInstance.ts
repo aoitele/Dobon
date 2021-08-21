8d18548806647c4733ba0da205a8a5b5e96c1e7f
Takuto Eto <aoitele@gmail.com>
2021-08-21 15:40:24 +0900

    axios baseURLも動的に

--- next-app/utils/api/axiosInstance.ts
+++ next-app/utils/api/axiosInstance.ts
@@ -4,7 +4,7 @@ import hasProperty from '../function/hasProperty';
 const axiosInstance = (options?: AxiosRequestConfig) => {
    
     const client = axios.create({
-        baseURL: process.env.NEXT_PUBLIC_API_SERVER_URL || 'http://localhost:3000',
+        baseURL: process.env.NEXT_PUBLIC_API_SERVER_URL || `http://0.0.0.0:${process.env.PORT}`,
         timeout: 3000,
         responseType: 'json'
     });
