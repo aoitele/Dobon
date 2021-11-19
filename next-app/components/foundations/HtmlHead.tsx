ced4a2d4f26896aed28c39102baa9ef1a801b98a
Takuto Eto <aoitele@gmail.com>
2021-11-20 00:02:14 +0900

    Add meta for PWA (#23)
    * Add Next/PWA
    
    * Add meta for PWA

--- next-app/components/foundations/HtmlHead.tsx
+++ next-app/components/foundations/HtmlHead.tsx
@@ -8,7 +8,7 @@ const HtmlHead: React.FC<Props> = ({ title }) => (
   <>
     <Head>
       <title>{title}</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
     </Head>
   </>
 )
