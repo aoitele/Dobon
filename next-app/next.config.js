589609f260486d948e8740076c1e730934b96a7b
Takuto Eto <aoitele@gmail.com>
2021-11-21 16:14:45 +0900

    test:serve pwa file from .next

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -4,7 +4,6 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === 'development',
-    dest: 'public'
   },
   productionBrowserSourceMaps: true,
   sassOptions: {
