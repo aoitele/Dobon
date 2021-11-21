2a1590574bc80e0128855d856ab9bee5224b40de
Takuto Eto <aoitele@gmail.com>
2021-11-21 15:33:19 +0900

    rename document.tsx

--- next-app/components/foundations/HtmlHead.tsx
+++ next-app/components/foundations/HtmlHead.tsx
@@ -9,8 +9,6 @@ const HtmlHead: React.FC<Props> = ({ title }) => (
     <Head>
       <title>{title}</title>
       <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
-      <link rel="manifest" href="/manifest.json" />
-      <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
     </Head>
   </>
 )
