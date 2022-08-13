d4eaee2284e05b1c655a32403498e186fde33e3c
Takuto Eto <aoitele@gmail.com>
2022-08-13 12:09:43 +0900

    Fix test culcDobonRisk using resRemainingCard

--- next-app/__tests__/modules/culcDobonRisk.spec.tsx
+++ next-app/__tests__/modules/culcDobonRisk.spec.tsx
@@ -2,7 +2,7 @@ import { HandCards } from '../../@types/card'
 import { OtherHands } from '../../@types/game'
 import { culcDobonRisk } from '../../utils/game/cpu/thinking/putout/culcDobonRisk'
 import { DobonRiskReturnValue, DOBONRISK_MAX, DOBONRISK_MEDIAN, DOBONRISK_MIN } from '../../utils/game/cpu/thinking/putout/main'
-import { DeckCards } from '../../utils/game/cpu/thinking/putout/resRemainingCard'
+import { DeckCards, resRemainingCard } from '../../utils/game/cpu/thinking/putout/resRemainingCard'
 
 describe('culcDobonRisk TestCases', () => {
   it('他ユーザーの公開カード合計値を出す場合はリスク最大となる(+101)', () => {
@@ -13,22 +13,8 @@ describe('culcDobonRisk TestCases', () => {
       {userId: 3, hands:['h4o', 'z', 'z', 'c11o']},
     ]
     const defineRiskCards: number[] = [9, 11]
-    const remainingCard:DeckCards = {
-      0:2,
-      1:3,
-      2:4,
-      3:4,
-      4:3,
-      5:4,
-      6:4,
-      7:4,
-      8:4,
-      9:3,
-      10:3,
-      11:3,
-      12:4,
-      13:4,
-    }
+    const trashedMemory:HandCards[] = []
+    const remainingCard:DeckCards = resRemainingCard(otherHands, trashedMemory)
 
     const expected: DobonRiskReturnValue = [
       { card: 'c9o', dobonRisk: DOBONRISK_MAX}
@@ -44,22 +30,8 @@ describe('culcDobonRisk TestCases', () => {
       {userId: 3, hands:['h4o', 'z', 'z', 'c11o']},
     ]
     const defineRiskCards: number[] = [9, 11]
-    const remainingCard:DeckCards = {
-      0:2,
-      1:2,
-      2:4,
-      3:3,
-      4:3,
-      5:3,
-      6:4,
-      7:4,
-      8:4,
-      9:3,
-      10:3,
-      11:3,
-      12:4,
-      13:4,
-    }
+    const trashedMemory:HandCards[] = []
+    const remainingCard:DeckCards = resRemainingCard(otherHands, trashedMemory)
 
     const expected: DobonRiskReturnValue = [
       { card: 'c9o', dobonRisk: DOBONRISK_MIN},
@@ -78,22 +50,9 @@ describe('culcDobonRisk TestCases', () => {
       {userId: 3, hands:['h4o', 'z', 'z', 'c11o']},
     ]
     const defineRiskCards: number[] = [9, 11]
-    const remainingCard:DeckCards = {
-      0:2,
-      1:3,
-      2:4,
-      3:4,
-      4:2,
-      5:3,
-      6:4,
-      7:4,
-      8:4,
-      9:3,
-      10:3,
-      11:3,
-      12:4,
-      13:4,
-    }
+    const trashedMemory:HandCards[] = []
+    const remainingCard:DeckCards = resRemainingCard(otherHands, trashedMemory)
+
     const expected: DobonRiskReturnValue = [
       { card: 'c9o', dobonRisk: DOBONRISK_MIN},
       { card: 'c4o', dobonRisk: DOBONRISK_MIN},
