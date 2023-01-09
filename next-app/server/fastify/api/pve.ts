c1723e270c4d47822f404650fe8cc78781626748
Takuto Eto <aoitele@gmail.com>
2023-01-09 16:00:44 +0900

    use randomUUID to pveKey

--- next-app/server/fastify/api/pve.ts
+++ next-app/server/fastify/api/pve.ts
@@ -1,9 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+const { randomUUID } = require("crypto");
 
 export const getPveKey = (_req: FastifyRequest, res: FastifyReply) => {
-  const date = new Date();
-  date.setTime(date.getTime() + (9*60*60*1000));
-  const str_date = date.toISOString().replace('T', ' ').substr(0, 19);
-  console.log(str_date);
-  res.send({ pveKey: `sample-pveKey-${str_date}` })
+  res.send({ pveKey: randomUUID() })
 }
