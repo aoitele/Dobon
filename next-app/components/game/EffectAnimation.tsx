910d4e47e34a211d2449e62a763abf87327d90d2
Takuto Eto <aoitele@gmail.com>
2023-03-21 17:23:07 +0900

    Add Effect SVG Components

--- next-app/components/game/EffectAnimation.tsx
+++ next-app/components/game/EffectAnimation.tsx
@@ -2,18 +2,35 @@ import React, { FC } from 'react'
 import Image from 'next/image'
 import style from './EffectAnimation.module.scss'
 import { ModalEffect, Player } from '../../@types/game'
+import { DobonEffect } from './effect/DobonEffect'
+import { Draw2Effect } from './effect/Draw2Effect'
+import { Draw4Effect } from './effect/Draw4Effect'
+import { Draw6Effect } from './effect/Draw6Effect'
+import { Draw8Effect } from './effect/Draw8Effect'
+import { JokerEffect } from './effect/JokerEffect'
+import { OpenCardEffect } from './effect/OpenCardEffect'
+import { SkipEffect } from './effect/SkipEffect'
+import { WildEffect } from './effect/WildEffect'
+import { ReverseEffect } from './effect/ReverseEffect'
 
 const effectAnimation:React.FC<ModalEffect> = ({ user, action, message }) => {
+  const isJokerEffect = action === 'joker'
   return (
     <>
       <div className={style.wrap}>
         <div className={style.imageWrap}>
-          <Image
-            src={`/images/game/effect/${action}.png`}
-            width={300}
-            height={300}
-            objectFit="contain"
-          />
+          <div className={isJokerEffect ? style.JokerEffectWrap : style.EffectWrap}>
+          {action === 'dobon'   && <DobonEffect/>}
+          {action === 'draw2'   && <Draw2Effect/>}
+          {action === 'draw4'   && <Draw4Effect/>}
+          {action === 'draw6'   && <Draw6Effect/>}
+          {action === 'draw8'   && <Draw8Effect/>}
+          {action === 'joker'   && <JokerEffect/>}
+          {action === 'opencard'&& <OpenCardEffect/>}
+          {action === 'skip'    && <SkipEffect/>}
+          {action === 'reverse' && <ReverseEffect/>}
+          {action === 'wild'    && <WildEffect/>}
+          </div>
         </div>
         <div className={style.imageBg}/>
       </div>
