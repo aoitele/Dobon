aad0852b33fe52b87d86f66ac39f96894ae32443
Takuto Eto <aoitele@gmail.com>
2023-05-06 23:16:03 +0900

    Add SelectCardInfo

--- next-app/components/pages/pve/components/container/MyInfoContainer.tsx
+++ next-app/components/pages/pve/components/container/MyInfoContainer.tsx
@@ -1,15 +1,24 @@
 import React, { useContext } from 'react'
+import { BoardStateContext } from '../../../../../context/BoardProvider'
 import { GameStateContext } from '../../../../../context/GameProvider'
+import { isEffectCard } from '../../../../../utils/game/effect'
+import SelectCardInfo from '../../../../game/SelectCardInfo'
 import UserInfo from '../../../../game/UserInfo'
 import styles from './MyInfoContainer.module.scss'
 
+/**
+ * selectingCardが効果カードのとき、UserInfoの領域はカード効果のヒントを表示させている
+ */
 const MyInfoContainer = () => {
-  const { board } = useContext(GameStateContext).game
-  const turnUser = board.users.find(user => user.turn === board.turn)
+  const [gameState, boardState] = [useContext(GameStateContext), useContext(BoardStateContext)]
+  const turnUser = gameState.game.board.users.find(user => user.turn === gameState.game.board.turn)
+  const isSelectingEffectCard = isEffectCard({ card:[boardState.selectedCard], isMyCard:true })
 
   return (
     <div className={styles.wrap}>
-      {turnUser && <UserInfo user={board.users[0]} turnUser={turnUser} />}
+      {isSelectingEffectCard
+      ? <SelectCardInfo states={{values: boardState}}/>
+      : turnUser && <UserInfo user={gameState.game.board.users[0]} turnUser={turnUser} />}
     </div>
   )
 }
